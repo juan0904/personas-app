e50c80a75740b10d3f2e9a8cbc46a3676d59233b
Juan Jose Sanchez <juanjsanchezcea@gmail.com>
2024-03-29 18:52:43 -0500

    Index Departamentos

---
+++ resources/views/departamento/index.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Listado de Departamentos</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Listado de Departamentos</h1>
+      <a href="" class="btn btn-success">Add</a>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Codigo</th>
+                <th scope="col">Departamento</th>
+                <th scope="col">Pais</th>
+                <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach( $departamentos as $departamento )
+                <tr>
+                <th scope="row">{{$departamento->depa_codi}}</th>
+                <td>{{$departamento->depa_nomb}}</td>
+                <td>{{$departamento->pais_codi}}</td>
+                <td>Actions</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    
+
+    
+   </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComunaController;
+use App\Http\Controllers\DepartamentoController;
 use App\Http\Controllers\MunicipioController;
 
 Route::get('/', function () {
@@ -24,3 +25,8 @@ Route::get('/municipios/create', [MunicipioController::class , 'create'])->name(
 Route::delete('/municipios/{municipio}',[MunicipioController::class,'destroy'])->name('municipios.destroy');
 Route::put('/municipios/{municipio}', [MunicipioController::class, 'update'])->name('municipios.update');
 Route::get('/municipios/{municipio}/edit', [MunicipioController::class , 'edit'])->name('municipios.edit');
+
+//rutas de departamentos
+
+Route::get('/departamentos', [DepartamentoController:: class, 'index'])->name('departamento.index');
+
